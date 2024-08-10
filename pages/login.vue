62d193bb703dd4d532aac89d67ca4e7eeced98cd
Lostjerome <naronkrach@gmail.com>
2024-08-11 00:08:00 +0700

    setup theme toggle

---
+++ pages/login.vue
@@ -0,0 +1,20 @@
+<template>
+  <ThemeToggle />
+    <div class="min-h-screen flex justify-end ">
+        <div class="max-w-screen-sm w-full m-6">
+            <Card class="h-full">
+                <template #title>Login</template>
+                <template #content>
+                    <p class="m-0">
+                        <Button>
+                          Continue with google
+                        </Button>
+                    </p>
+                </template>
+            </Card>
+        </div>
+    </div>
+</template>
+<script setup>
+const password = ref("");
+</script>
