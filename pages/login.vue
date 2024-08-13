6295593c799ce56cba501eea8c5e0579d2bd3ffb
Lostjerome <naronkrach@gmail.com>
2024-08-13 13:16:45 +0700

    Create layout

--- pages/login.vue
+++ pages/login.vue
@@ -1,20 +1,19 @@
-<template>
-  <ThemeToggle />
-    <div class="min-h-screen flex justify-end ">
-        <div class="max-w-screen-sm w-full m-6">
-            <Card class="h-full">
-                <template #title>Login</template>
-                <template #content>
-                    <p class="m-0">
-                        <Button>
-                          Continue with google
-                        </Button>
-                    </p>
-                </template>
-            </Card>
-        </div>
-    </div>
-</template>
-<script setup>
+<script setup lang="js">
 const password = ref("");
 </script>
+
+<template>
+        <ThemeToggle />
+        <div class="min-h-screen flex justify-end">
+            <div class="max-w-screen-sm w-full m-6">
+                <Card class="h-full">
+                    <template #title>Login</template>
+                    <template #content>
+                        <p class="m-0">
+                            <Button> Continue with google </Button>
+                        </p>
+                    </template>
+                </Card>
+            </div>
+        </div>
+</template>
