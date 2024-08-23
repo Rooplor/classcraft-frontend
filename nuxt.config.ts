ee7abdc014299c00eb83325a155ad1dc9b2803d7
Lostjerome <naronkrach@gmail.com>
2024-08-23 13:29:32 +0700

    Install vee validate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,22 @@ import Aura from "@primevue/themes/aura";
 export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
-    modules: ["@primevue/nuxt-module", "@nuxtjs/tailwindcss"],
+    modules: [
+        "@primevue/nuxt-module",
+        "@nuxtjs/tailwindcss",
+        "@vee-validate/nuxt",
+    ],
+    veeValidate: {
+        // disable or enable auto imports
+        autoImports: true,
+        // Use different names for components
+        componentNames: {
+            Form: "VeeForm",
+            Field: "VeeField",
+            FieldArray: "VeeFieldArray",
+            ErrorMessage: "VeeErrorMessage",
+        },
+    },
     primevue: {
         options: {
             theme: {
