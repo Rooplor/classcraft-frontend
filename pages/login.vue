34f2671d13c8f6ddcde8667eb20acdee01bea7ad
Lostjerome <naronkrach@gmail.com>
2024-09-13 14:25:21 +0700

    Adjust UI

--- pages/login.vue
+++ pages/login.vue
@@ -1,11 +1,14 @@
 <script setup lang="js">
+definePageMeta({
+  layout: false,
+});
 const password = ref("");
 </script>
 
 <template>
         <ThemeToggle />
-        <div class="min-h-screen flex justify-end">
-            <div class="max-w-screen-sm w-full m-6">
+        <div class="flex justify-end min-h-screen">
+            <div class="w-full max-w-screen-sm m-6">
                 <Card class="h-full">
                     <template #title>Login</template>
                     <template #content>
