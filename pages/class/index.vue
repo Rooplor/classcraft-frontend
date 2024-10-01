5308021440e2efe7cd33db4e01ae0be4a28af101
Lostjerome <naronkrach@gmail.com>
2024-10-01 22:20:01 +0700

    Change class file location

---
+++ pages/class/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useClassroomStore } from "../../stores/classroom";
+
+const classroomStore = useClassroomStore();
+const { getAllClassroom } = useClassroom();
+
+const classes = ref([]);
+
+const getClassroom = async () => {
+    classes.value = await getAllClassroom();
+    classroomStore.setClassroom(classes.value);
+};
+</script>
+
+<template>
+    <div>
+        <p>Classroom: {{ classroomStore.classroom }}</p>
+
+        <button @click="getClassroom">Get Classroom</button>
+    </div>
+</template>
