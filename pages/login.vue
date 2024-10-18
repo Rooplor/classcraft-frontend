420c8b8585ca1c1d1d0a369757f320cf7aa3e114
Lostjerome <naronkrach@gmail.com>
2024-10-18 12:59:59 +0700

    setup login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,22 +1,32 @@
-<script setup lang="js">
-definePageMeta({
-  layout: false,
-});
-const password = ref("");
-</script>
+<script setup lang="ts">
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
+definePageMeta({
+    layout: false,
+});
+
+const auth = useFirebaseAuth();
+const router = useRouter();
+
+const signInWithGoogle = () => {
+    signInWithPopup(auth, new GoogleAuthProvider()).then(() => {
+        router.push("/");
+    });
+};
+</script>
 <template>
-        <ThemeToggle />
-        <div class="flex justify-end min-h-screen">
-            <div class="w-full max-w-screen-sm m-6">
-                <Card class="h-full">
-                    <template #title>Login</template>
-                    <template #content>
-                        <p class="m-0">
-                            <Button> Continue with google </Button>
-                        </p>
-                    </template>
-                </Card>
-            </div>
+    <div class="min-h-screen flex justify-end">
+        <div class="max-w-screen-sm w-full m-6">
+            <Card class="h-full">
+                <template #title>Login</template>
+                <template #content>
+                    <p class="m-0">
+                        <Button @click="signInWithGoogle">
+                            Continue with google
+                        </Button>
+                    </p>
+                </template>
+            </Card>
         </div>
+    </div>
 </template>
