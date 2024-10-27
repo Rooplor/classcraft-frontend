937f58dedf3df2291f6578a8097e8f90b19b4088
Lostjerome <naronkrach@gmail.com>
2024-10-27 15:06:27 +0700

    Add classroom api functions

--- composables/useClassroom.ts
+++ composables/useClassroom.ts
@@ -13,16 +13,25 @@ const useClassroom = () => {
         });
     };
 
+    const getClassroomById = (id: string): Promise<IClassroom> => {
+        return $fetch(`${config.public.baseUrl}/api/class/${id}`, {
+            method: "GET",
+        });
+    };
+
+    const getClassroomByUserId = (userId: string[]): Promise<IClassroom[]> => {
+        return $fetch(`${config.public.baseUrl}/api/class/owners`, {
+            params: {
+                userId,
+            },
+            method: "GET",
+        });
+    };
+
     const addClassroom = (classroom: IAddClassroomDTO): Promise<IClassroom> => {
         return $fetch(`${config.public.baseUrl}/api/class`, {
             method: "POST",
             body: JSON.stringify(classroom),
-        });
-    };
-
-    const getClassroomById = (id: string): Promise<IClassroom> => {
-        return $fetch(`${config.public.baseUrl}/api/class/${id}`, {
-            method: "GET",
         });
     };
 
@@ -41,12 +50,80 @@ const useClassroom = () => {
             method: "DELETE",
         });
     };
+
+    const updateVenue = (id: string, venue: string): Promise<IClassroom> => {
+        return $fetch(
+            `${config.public.baseUrl}/api/class/${id}/venue/${venue}`,
+            {
+                method: "PATCH",
+            }
+        );
+    };
+
+    const toggleRegistrationStatus = (id: string): Promise<IClassroom> => {
+        return $fetch(
+            `${config.public.baseUrl}/api/class/${id}/toggle-registration-status`,
+            {
+                method: "PATCH",
+            }
+        );
+    };
+
+    const togglePublishStatus = (id: string): Promise<IClassroom> => {
+        return $fetch(
+            `${config.public.baseUrl}/api/class/${id}/toggle-publish-status`,
+            {
+                method: "PATCH",
+            }
+        );
+    };
+
+    const updateRegistrationUrl = (
+        id: string,
+        registrationUrl: string
+    ): Promise<IClassroom> => {
+        return $fetch(
+            `${config.public.baseUrl}/api/class/${id}/registration-url`,
+            {
+                body: registrationUrl,
+                method: "PATCH",
+            }
+        );
+    };
+
+    const updateMeetingUrl = (
+        id: string,
+        meetingUrl: string
+    ): Promise<IClassroom> => {
+        return $fetch(`${config.public.baseUrl}/api/class/${id}/meeting-url`, {
+            body: meetingUrl,
+            method: "PATCH",
+        });
+    };
+
+    const updateContent = (
+        id: string,
+        content: string
+    ): Promise<IClassroom> => {
+        return $fetch(`${config.public.baseUrl}/api/class/${id}/content`, {
+            body: content,
+            method: "PATCH",
+        });
+    };
+
     return {
         getAllClassroom,
-        addClassroom,
         getClassroomById,
+        getClassroomByUserId,
+        addClassroom,
         updateClassroom,
         deleteClassroom,
+        updateVenue,
+        toggleRegistrationStatus,
+        togglePublishStatus,
+        updateRegistrationUrl,
+        updateMeetingUrl,
+        updateContent,
     };
 };
 
