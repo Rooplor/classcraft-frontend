186b01778e4129bfe6e082718db8dcca1770574e
Natdanai Nitipan <c3bosskung@gmail.com>
2024-11-07 20:00:49 +0700

    add baseUrl for support reverse proxy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,6 +25,9 @@ export default defineNuxtConfig({
     compatibilityDate: "2024-04-03",
     devtools: { enabled: true },
     ssr: false,
+    app: {
+        baseURL: '/kp2'
+    },
     modules: [
         "@primevue/nuxt-module",
         "@nuxtjs/tailwindcss",
