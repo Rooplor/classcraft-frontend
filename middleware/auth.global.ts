ea9edeafed9bcbfa632382cf85dacd818efebec8
Lostjerome <naronkrach@gmail.com>
2024-12-02 16:05:36 +0700

    chore: Update auth.global middleware to skip authentication for reservation routes

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -5,6 +5,10 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
         return navigateTo("/");
     }
 
+    if (!user && to.path.includes("/reservation/")) {
+        return;
+    }
+
     if (!user && to.name !== "login") {
         return navigateTo("/login");
     }
