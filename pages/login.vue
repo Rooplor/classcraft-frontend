d673dc180b452113e948dbbaec56f4bc1b7ca1ee
Lostjerome <naronkrach@gmail.com>
2024-12-02 16:20:12 +0700

    chore: Update login page UI

--- pages/login.vue
+++ pages/login.vue
@@ -20,18 +20,23 @@ const signInWithGoogle = async () => {
 };
 </script>
 <template>
-    <div class="min-h-screen flex justify-end">
-        <div class="max-w-screen-sm w-full m-6">
-            <Card class="h-full">
-                <template #title>Login</template>
-                <template #content>
-                    <p class="m-0">
-                        <Button @click="signInWithGoogle">
-                            Continue with google
-                        </Button>
-                    </p>
-                </template>
-            </Card>
+    <div
+        class="min-h-screen flex justify-end bg-gradient-to-br from-primary to-indigo-600"
+    >
+        <div
+            class="flex flex-col justify-center max-w-sm w-full bg-white rounded-xl shadow-lg p-6 m-2"
+        >
+            <h1 class="text-4xl font-bold text-gray-800 text-center mb-8">
+                Welcome
+            </h1>
+            <p class="m-0 text-center">
+                <button
+                    @click="signInWithGoogle"
+                    class="w-full flex items-center gap-2 justify-center py-4 bg-blue-600 text-white font-semibold rounded-full hover:bg-blue-700 transition-all duration-200"
+                >
+                    <i class="pi pi-google" /> Continue with Google
+                </button>
+            </p>
         </div>
     </div>
 </template>
