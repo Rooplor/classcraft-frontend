b0c1f57b8cc034692d1f5601a272df6232a4f829
Lostjerome <naronkrach@gmail.com>
2024-12-08 23:37:55 +0700

    refactor: Update Feedbar and page layouts for better responsiveness

--- pages/discussion/index.vue
+++ pages/discussion/index.vue
@@ -6,26 +6,28 @@ const options = ref(["Pending", "Closed"]);
 </script>
 
 <template>
-    <div class="w-full max-w-screen-lg mx-auto px-2">
+    <div class="w-full">
         <Feedbar />
-        <div class="flex flex-col gap-4 pt-6 pb-[10px]">
-            <div class="flex justify-between">
-                <SelectButton
-                    v-model="value"
-                    :options="options"
-                    aria-labelledby="basic"
-                />
+        <div class="w-full max-w-screen-lg mx-auto px-2">
+            <div class="flex flex-col gap-4 pt-6 pb-[10px]">
+                <div class="flex justify-between">
+                    <SelectButton
+                        v-model="value"
+                        :options="options"
+                        aria-labelledby="basic"
+                    />
 
-                <Button severity="info">
-                    <p><i class="pi pi-plus" /> Add discussion</p>
-                </Button>
-            </div>
-            <div class="space-y-[10px]">
-                <DiscussionItem
-                    v-for="(discussion, index) in mockDiscussions"
-                    :key="index"
-                    :discussion="discussion"
-                />
+                    <Button severity="info">
+                        <p><i class="pi pi-plus" /> Add discussion</p>
+                    </Button>
+                </div>
+                <div class="space-y-[10px]">
+                    <DiscussionItem
+                        v-for="(discussion, index) in mockDiscussions"
+                        :key="index"
+                        :discussion="discussion"
+                    />
+                </div>
             </div>
         </div>
     </div>
