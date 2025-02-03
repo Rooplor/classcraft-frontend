44162f423ae9cb537931d781a968965d9b6521fc
Lostjerome <naronkrach@gmail.com>
2025-02-03 13:07:52 +0700

    Bypass user authentication when routing to specific classroom page

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,15 +1,19 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    const user = await getCurrentUser();
+  const user = await getCurrentUser();
 
-    if (user && to.name === "login") {
-        return navigateTo("/");
-    }
+  if (!user && to.path.match(/\/class\/[a-zA-Z0-9]+$/)) {
+    return;
+  }
 
-    if (!user && to.path.includes("/reservation/")) {
-        return;
-    }
+  if (user && to.name === "login") {
+    return navigateTo("/");
+  }
 
-    if (!user && to.name !== "login") {
-        return navigateTo("/login");
-    }
+  if (!user && to.path.includes("/reservation/")) {
+    return;
+  }
+
+  if (!user && to.name !== "login") {
+    return navigateTo("/login");
+  }
 });
