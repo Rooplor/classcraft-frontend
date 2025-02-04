82e1a32a93f5a747b68b552537651cf5ed73f6d0
Lostjerome <naronkrach@gmail.com>
2025-02-04 12:09:54 +0700

    feat: update classroom registration display logic to prioritize owner visibility

--- pages/class/[id]/index.vue
+++ pages/class/[id]/index.vue
@@ -362,11 +362,8 @@ useHead({
                 {{ usersInClassroom.length }} people joined this class
               </p>
             </div>
-            <div
-              v-else-if="
-                classroom.registrationStatus && user.id !== classroom.owner
-              "
-            >
+            <div v-else-if="user.id === classroom.owner" />
+            <div v-else-if="classroom.registrationStatus">
               <p class="text-slate-500">Be the first to join this class</p>
             </div>
           </div>
