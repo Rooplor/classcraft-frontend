f62c8c12884add8d9c5f748257f39c73f7216915
nnitipan <natdanai.nitipan@agoda.com>
2025-02-15 16:35:54 +0700

    fix update-class test

--- e2e/classroomTest/update-class.spec.ts
+++ e2e/classroomTest/update-class.spec.ts
@@ -13,14 +13,14 @@ test.beforeEach(async ({page}) => {
 test("should be update class title", async ({page}) => {
     await updateClassResponse('notUpdatedClassResponse.json', 'updatedClassTitleResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+    await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
     await page.getByText("Fill class detail").nth(0).click();
     await page.getByPlaceholder('Class title').fill("1. Cybersecurity Essentials for the Digital World (EDITED)");
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await expect(page.getByText("Class updated")).toBeVisible();
 });
@@ -28,14 +28,14 @@ test("should be update class title", async ({page}) => {
 test("should be update class details", async ({page}) => {
     await updateClassResponse('updatedClassTitleResponse.json', 'updatedClassDetailResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
-    await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
-    await page.getByText("Fill class detail").nth(0).click();
-    await page.locator('#details div').nth(2).fill("This lecture covers fundamental cybersecurity principles, common threats, and practical strategies to protect digital assets. (edited)");
+    await page.getByRole('tab', { name: 'Fill class detail' }).click();
+    await page.getByPlaceholder('Enter class details').click();
+    await page.getByPlaceholder('Enter class details').fill("This lecture covers fundamental cybersecurity principles, common threats, and practical strategies to protect digital assets. (edited)");
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await expect(page.getByText("Class updated")).toBeVisible();
 })
@@ -43,14 +43,14 @@ test("should be update class details", async ({page}) => {
 test("should be update class target", async ({page}) => {
     await updateClassResponse('updatedClassDetailResponse.json', 'updatedClassTargetResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
     await page.getByText("Fill class detail").nth(0).click();
     await page.getByPlaceholder('Who should attend this class?').fill('IT professionals, students, and small business owners (edited)');
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await expect(page.getByText("Class updated")).toBeVisible();
 })
@@ -58,7 +58,7 @@ test("should be update class target", async ({page}) => {
 test("should be update class prerequisites", async ({page}) => {
     await updateClassResponse('updatedClassTargetResponse.json', 'updatedClassPrerequisitesResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
@@ -66,7 +66,7 @@ test("should be update class prerequisites", async ({page}) => {
     await page.getByPlaceholder('What should attendees know').click();
     await page.getByPlaceholder('What should attendees know').fill('Basic knowledge of IT systems (edited)');
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await expect(page.getByText("Class updated")).toBeVisible();
 })
@@ -74,14 +74,14 @@ test("should be update class prerequisites", async ({page}) => {
 test("should be update class type", async ({page}) => {
     await updateClassResponse('updatedClassPrerequisitesResponse.json', 'updatedClassTypeResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
     await page.getByText("Fill class detail").nth(0).click();
     await page.getByRole('button', {name: 'Workshop'}).click();
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await expect(page.getByText("Class updated")).toBeVisible();
 })
@@ -89,14 +89,14 @@ test("should be update class type", async ({page}) => {
 test("should be update class format", async ({page}) => {
     await updateClassResponse('updatedClassTypeResponse.json', 'updatedClassFormatResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
     await page.getByText("Fill class detail").nth(0).click();
     await page.getByRole('button', {name: 'Online'}).click();
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await expect(page.getByText("Class updated")).toBeVisible();
 })
@@ -104,7 +104,7 @@ test("should be update class format", async ({page}) => {
 test("should be update class available seats", async ({page}) => {
     await updateClassResponse('updatedClassFormatResponse.json', 'updatedClassSeatResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
@@ -112,7 +112,7 @@ test("should be update class available seats", async ({page}) => {
 
     await page.getByPlaceholder('How many people can attend?').fill('40');
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await expect(page.getByText("Class updated")).toBeVisible();
 })
@@ -120,20 +120,20 @@ test("should be update class available seats", async ({page}) => {
 test("should be update class date and time", async ({page}) => {
     await updateClassResponse('updatedClassSeatResponse.json', 'updatedClassDateResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
-    await page.getByText("Fill class detail").nth(0).click();
+    await page.getByRole('tab', { name: 'Fill class detail' }).click();
 
     await page.getByPlaceholder('Select date').click();
     await page.getByPlaceholder('Select date').fill('Sun 6 Dec 2024');
     await page.getByPlaceholder('Select start time').click();
-    await page.getByPlaceholder('Select start time').fill('09:00');
+    await page.getByLabel('Next Hour').first().click();
     await page.getByPlaceholder('Select end time').click();
-    await page.getByPlaceholder('Select end time').fill('17:00');
+    await page.getByLabel('Next Hour').nth(1).click();
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await expect(page.getByText("Class updated")).toBeVisible();
 })
@@ -141,7 +141,7 @@ test("should be update class date and time", async ({page}) => {
 test("should be update class instructor about", async ({page}) => {
     await updateClassResponse('updatedClassDateResponse.json', 'updatedClassInstructorAboutResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
@@ -150,7 +150,7 @@ test("should be update class instructor about", async ({page}) => {
     await page.getByPlaceholder('Tell us about the instructor').click();
     await page.getByPlaceholder('Tell us about the instructor').fill("edited@example.com");
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await expect(page.getByText("Class updated")).toBeVisible();
 })
@@ -158,7 +158,7 @@ test("should be update class instructor about", async ({page}) => {
 test("should be update class instructor familiarity", async ({page}) => {
     await updateClassResponse('updatedClassInstructorAboutResponse.json', 'updatedClassInstructorFamResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
@@ -167,7 +167,7 @@ test("should be update class instructor familiarity", async ({page}) => {
     await page.getByPlaceholder('How familiar is the').click();
     await page.getByPlaceholder('How familiar is the').fill("SIT STUDENT (edited)");
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await expect(page.getByText("Class updated")).toBeVisible();
 })
@@ -175,40 +175,40 @@ test("should be update class instructor familiarity", async ({page}) => {
 test("should be update publish class", async ({page}) => {
     await updateClassResponse('updatedClassInstructorFamResponse.json', 'updatedClassPublishedResponse.json', page)
 
-    await page.route("http://localhost:8080/api/class/1/toggle-publish-status", route => {
+    await page.route("http://localhost:8080/api/class/1/set-publish-status", route => {
         route.fulfill({
             path: './e2e/utils/mockResponse/updatedResponse/updatedClassPublishedResponse.json',
         })
     })
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+    await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
-    await page.getByText("Fill class detail").nth(0).click();
+    await page.getByRole('tab', { name: 'Fill class detail' }).click();
 
-    await page.getByLabel('Share').click();
-    await page.getByLabel('Publish').click();
+    await page.getByRole('button', { name: '' }).click();
+    await page.getByLabel('Publish').nth(1).click();
 
-    await expect(page.getByText("Classroom is published")).toBeVisible();
+    await expect(page.getByRole('alert')).toContainText('Classroom is published');
 })
 
 test("should be update unpublish class", async ({page}) => {
     await updateClassResponse('updatedClassPublishedResponse.json', 'updatedClassUnPublishedResponse.json', page)
 
-    await page.route("http://localhost:8080/api/class/1/toggle-publish-status", route => {
+    await page.route("http://localhost:8080/api/class/1/set-publish-status", route => {
         route.fulfill({
             path: './e2e/utils/mockResponse/updatedResponse/updatedClassUnPublishedResponse.json',
         })
     })
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+    await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
-    await page.getByText("Fill class detail").nth(0).click();
+    await page.getByRole('tab', { name: 'Fill class detail' }).click();
 
-    await page.getByLabel('Share').click();
+    await page.getByRole('button', { name: '' }).click();
     await page.getByLabel('Unpublish').click();
 
     await expect(page.getByText("Classroom is unpublished")).toBeVisible();
@@ -217,7 +217,7 @@ test("should be update unpublish class", async ({page}) => {
 test("should not be update title", async ({page}) => {
     await updateClassResponse('updatedClassPublishedResponse.json', 'updatedClassUnPublishedResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
@@ -226,7 +226,7 @@ test("should not be update title", async ({page}) => {
     await page.getByText("Fill class detail").nth(0).click();
     await page.getByPlaceholder('Class title').fill("");
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await showValidationMsg(page, validateMsg, ['titleRequired'], true);
 })
@@ -234,16 +234,15 @@ test("should not be update title", async ({page}) => {
 test("should not be update detail", async ({page}) => {
     await updateClassResponse('updatedClassPublishedResponse.json', 'updatedClassUnPublishedResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
-    await page.getByText("Fill class detail").nth(0).click();
+    await page.getByRole('tab', { name: 'Fill class detail' }).click();
+    await page.getByPlaceholder('Enter class details').click();
+    await page.getByPlaceholder('Enter class details').fill("");
 
-    await page.getByText("Fill class detail").nth(0).click();
-    await page.locator('#details div').nth(2).fill("");
-
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await showValidationMsg(page, validateMsg, ['detailRequired'], true);
 })
@@ -251,7 +250,7 @@ test("should not be update detail", async ({page}) => {
 test("should not be update target", async ({page}) => {
     await updateClassResponse('updatedClassPublishedResponse.json', 'updatedClassUnPublishedResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
@@ -259,7 +258,7 @@ test("should not be update target", async ({page}) => {
 
     await page.getByPlaceholder('Who should attend this class?').fill('');
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await showValidationMsg(page, validateMsg, ['targetRequired'], true);
 })
@@ -267,7 +266,7 @@ test("should not be update target", async ({page}) => {
 test("should not be update available seats", async ({page}) => {
     await updateClassResponse('updatedClassPublishedResponse.json', 'updatedClassUnPublishedResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
@@ -275,7 +274,7 @@ test("should not be update available seats", async ({page}) => {
 
     await page.getByPlaceholder('How many people can attend?').fill("");
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await showValidationMsg(page, validateMsg, ['capacityRequired'], true);
 })
@@ -283,20 +282,18 @@ test("should not be update available seats", async ({page}) => {
 test("should not be update date time", async ({page}) => {
     await updateClassResponse('updatedClassPublishedResponse.json', 'updatedClassUnPublishedResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
-    await page.getByText("Fill class detail").nth(0).click();
+    await page.getByRole('tab', { name: 'Fill class detail' }).click();
 
     await page.getByPlaceholder('Select date').click();
     await page.getByPlaceholder('Select date').fill('');
     await page.getByPlaceholder('Select start time').click();
-    await page.getByPlaceholder('Select start time').fill('');
     await page.getByPlaceholder('Select end time').click();
-    await page.getByPlaceholder('Select end time').fill('');
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await showValidationMsg(page, validateMsg, ['dateTimeRequired'], true);
 })
@@ -304,7 +301,7 @@ test("should not be update date time", async ({page}) => {
 test("should not be update Instructor about", async ({page}) => {
     await updateClassResponse('updatedClassPublishedResponse.json', 'updatedClassUnPublishedResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
@@ -313,7 +310,7 @@ test("should not be update Instructor about", async ({page}) => {
     await page.getByPlaceholder('Tell us about the instructor').click();
     await page.getByPlaceholder('Tell us about the instructor').fill("");
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await showValidationMsg(page, validateMsg, ['instructorBioRequired'], true);
 })
@@ -321,7 +318,7 @@ test("should not be update Instructor about", async ({page}) => {
 test("should not be update Instructor familiarity", async ({page}) => {
     await updateClassResponse('updatedClassPublishedResponse.json', 'updatedClassUnPublishedResponse.json', page)
 
-    await page.getByText("1. Cybersecurity Essentials for the Digital World").click();
+await page.goto("http://localhost:3000/class/1/edit");
 
     await expect(page).toHaveURL("http://localhost:3000/class/1/edit");
     await expect(page.getByText("Fill class detail").nth(1)).toBeVisible();
@@ -330,7 +327,7 @@ test("should not be update Instructor familiarity", async ({page}) => {
     await page.getByPlaceholder('How familiar is the').click();
     await page.getByPlaceholder('How familiar is the').fill("");
 
-    await page.getByRole('button', {name: 'Save'}).click();
+    await page.getByLabel('Update').click();
 
     await showValidationMsg(page, validateMsg, ['instructorFamiliarityRequired'], true);
 })
