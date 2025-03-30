f7eb062bf0058fc01f91da94da2b329da9566da6
Lostjerome <naronkrach@gmail.com>
2025-03-30 20:40:59 +0700

    feat: Update wishlist page to use dynamic classroom requests and enhance type definitions

--- pages/wishlist/index.vue
+++ pages/wishlist/index.vue
@@ -4,17 +4,33 @@ import type { IClassroomRequest } from "../../types/ClassroomRequest";
 const { getUserClassroomRequests } = useRequestClassroom();
 const toast = useToast();
 let classroomRequests = ref<IClassroomRequest[]>([]);
-const mockclassroomRequests = [
-  {
-    title: "test",
-    coverImage: "",
-    format: "",
-    type: "",
-    capacity: "",
-    instructorName: "",
-    instructorAvatar: "",
-  },
-];
+// const classroomRequests = [
+//   {
+//     id: "67e926a45c16f001b3eb4d07",
+//     classroomId: "67e1b3ce191ead32464ca1ed",
+//     classroomDetail: {
+//       coverImage: null,
+//       title: "",
+//       format: "ONSITE",
+//       type: "LECTURE",
+//       capacity: 0,
+//       instructorName: "",
+//       instructorAvatar: "",
+//     },
+//     ownerId: "673dd60e10c4ce4a4bcfbf84",
+//     requestList: [
+//       {
+//         requestedBy: {
+//           id: "674694ef32ddea4012d3c50b",
+//           username: "Jerome",
+//           profilePicture:
+//             "https://lh3.googleusercontent.com/a/ACg8ocJNZD_DTWEev6zFRzznO7RKbXsE_KjO9htM8y5Nt44thxNOBWdZiA=s96-c",
+//         },
+//         requestedAt: "2025-03-30T11:10:28.336",
+//       },
+//     ],
+//   },
+// ];
 
 try {
   let res = await getUserClassroomRequests();
@@ -37,9 +53,9 @@ try {
         <h1 class="text-3xl font-bold">Wishlist</h1>
         <div class="space-y-2">
           <ClassroomRequestListItem
-            v-for="(classroom, index) in mockclassroomRequests"
+            v-for="(request, index) in classroomRequests"
             :key="index"
-            :classroom="classroom"
+            :classroom="request.classroomDetail"
           />
           <div
             v-if="classroomRequests?.length === 0"
