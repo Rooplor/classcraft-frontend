ef095c5126e70d50967e47272748f15ce6933760
Lostjerome <naronkrach@gmail.com>
2025-03-31 15:57:47 +0700

    style: Update ClassMaterial item styles for improved hover effects

--- components/ClassMaterial/Item.vue
+++ components/ClassMaterial/Item.vue
@@ -18,15 +18,15 @@ const emit = defineEmits(["remove"]);
   >
     <div class="relative flex justify-between items-center gap-2">
       <div
-        class="absolute top-4 right-4 bg-primary-100 border border-primary-300 rounded-lg w-32 aspect-square group-hover:-translate-y-2 group-hover:-rotate-3 duration-200"
+        class="absolute top-4 right-4 bg-primary-100 border border-primary-300 rounded-lg w-32 aspect-square group-hover:-translate-y-2 group-hover:-rotate-3 group-hover:bg-primary-200 duration-200"
       ></div>
       <div
-        class="absolute top-4 right-2 bg-primary-100 border border-primary-300 rounded-lg w-32 aspect-square rotate-6 group-hover:-translate-y-1 group-hover:rotate-[8deg] duration-200"
+        class="absolute top-4 right-2 bg-primary-100 border border-primary-300 rounded-lg w-32 aspect-square rotate-6 group-hover:-translate-y-1 group-hover:bg-primary-200 group-hover:rotate-[8deg] duration-200"
       ></div>
 
       <div class="flex gap-4 items-center z-10 truncate">
         <i
-          class="pi pi-file text-primary-500 bg-primary-100 p-2 rounded border border-primary-300"
+          class="pi pi-file text-primary-500 bg-primary-100 p-2 rounded border border-primary-300 group-hover:bg-primary-200 duration-200"
         />
         <p class="truncate">
           {{ getFileNameFromUrl(file) }}
